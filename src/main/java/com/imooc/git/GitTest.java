1629d742e639072727ec70f610f4c561bbfbb83f
Ltx <1010389523@qq.com>
2021-11-18 18:24:17 +0800

    hello git

---
+++ src/main/java/com/imooc/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.imooc.git;
+
+/**
+ * @author ltx
+ * @create 2021-11-18 18:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
